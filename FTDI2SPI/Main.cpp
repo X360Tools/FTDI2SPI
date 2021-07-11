b85d50da9e8ba90f0cd8bbc7feadb80febb62da4
Josh Davidson <joshuadavidson2000@gmail.com>
2021-07-10 23:47:16 -0400

    rework it a bunch

--- FTDI2SPI/Main.cpp
+++ FTDI2SPI/Main.cpp
@@ -4,283 +4,210 @@
 #include "wrapper_spi.h"
 #include "Flasher.h"
 #include "sfc.h"
+#include <sys/stat.h>
+#include <string>
 
 //#include <process.h>
 //#include <stdio.h>
 
-#define APP_NAME "Squirter" 
-#define APP_VERSION "J-Runner" 
-
 #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
 #define MAX(a, b)  (((a) > (b)) ? (a) : (b))
 
-#define PRINT_PROGRESS printf( "\r%0.1f%%", ((block_flash-StartBlock)*100)/(float)(block_flash_max-StartBlock) ) // unused, but lets leave it here
+// unused, but lets leave it here
+#define PRINT_PROGRESS printf( "\r%0.1f%%", ((block_flash-StartBlock)*100)/(float)(block_flash_max-StartBlock) )
 #define PRINT_BLOCKS printf( "\rBlock: %X", (block_flash/32))
                      
 #include <iostream>
 #include <fstream>
 using namespace std;
 
+unsigned int block_flash = 0;
 unsigned int block_size;
+unsigned int flashConfig = 0;
 
-int _tmain( int argc, _TCHAR* argv[] )
-{	
-	unsigned char *flash_xbox;
-	unsigned int addr_raw = 0;
-	unsigned int StartBlock = 0;
-	unsigned int CountBlock = 0;
-	unsigned int block_flash = 0;
-	unsigned int block_flash_max = 32768;
+extern "C"
+{
+	__declspec(dllexport) int spi(int mode, int size, char* file) {
+		unsigned char* flash_xbox;
+		unsigned int addr_raw = 0;
+		unsigned int StartBlock = 0;
+		unsigned int CountBlock = 0;
+		block_flash = 0;
+		unsigned int block_flash_max = 2048 * size;
 
-	bool Wrong_arg = false;
-	bool EraseEnable = false;
-	bool ReadEnable = false;
-	bool WriteEnable = false;
-	bool PatchECCEnable = false;
-	unsigned int SizeMb = 0;
-	char FileName[256] = "";
+		bool Wrong_arg = false;
+		bool EraseEnable = false;
+		bool ReadEnable = false;
+		bool WriteEnable = false;
+		bool PatchECCEnable = false;
+		string FileName = file;
 
-	printf( "%s ", APP_NAME );
-	printf( "v%s\n", APP_VERSION );
-	
-	for( int i = 0; i < argc && i<=5 ; i++ ) {
-		switch( i ) {
-			case 0:
-				break;
-			case 1:
-				if( strncmp( argv[i], "ftdi:", 4 ) == 0 ) {
-				}
-				else {
-					Wrong_arg = true;
-				}
-				break;
-			case 2:
-				if( strncmp( argv[i], "-r", 2 ) == 0 ) {
-					block_size = 0x210;
-					ReadEnable = true;
-				}
-				else if( strncmp( argv[i], "-R", 2 ) == 0 ) {
-					block_size = 0x200;
-					ReadEnable = true;
-				}
-				else if( strncmp( argv[i], "-w", 2 ) == 0 ) {
-					block_size = 0x210;
-					WriteEnable = true;
-				}
-				else if( strncmp( argv[i], "-e", 2 ) == 0 ) {
-					EraseEnable = true;
-				}
-	
-				else if( strncmp( argv[i], "+w", 2 ) == 0 ) {
-					PatchECCEnable = true;
-					block_size = 0x210;
-					WriteEnable = true;				
-				}
-	
-				else {
-					Wrong_arg = true;
-					break;
-				}
-
-				if( isdigit(argv[i][2]) )
-					SizeMb = atoi( &argv[i][2] );
-				else {
-					Wrong_arg = true;					
-				}
-
-				block_flash_max = 2048 * SizeMb;
-				break;
-
-			case 3:
-				strncpy_s( FileName, argv[i], sizeof(FileName) );
-				break;
-
-			case 4:
-				if( isdigit(argv[i][0]) ) {
-					if( sscanf( &argv[i][0], "%x", &StartBlock ) == 0 ) {
-						Wrong_arg = true;					
-					}
-					StartBlock *= 32;
-				}
-				else {
-					Wrong_arg = true;					
-				}
-
-				break;
-			case 5:
-				if( isdigit(argv[i][0]) ) {
-					if( sscanf( &argv[i][0], "%x", &CountBlock ) == 0 ) {
-						Wrong_arg = true;					
-					}
-					else {
-						block_flash_max = StartBlock + (CountBlock*32);
-					}
-				}
-				else {
-					Wrong_arg = true;					
-				}
-				break;
-
-			default:
-				Wrong_arg = true;
-				break;
+		if (mode == 0) { // Get Flash Config Only
+			block_size = 0x210;
+			ReadEnable = true;
+			StartBlock = 0;
+			block_flash_max = 32;
+		}
+		else if (mode == 1) { // -r
+			block_size = 0x210;
+			ReadEnable = true;
+		}
+		else if (mode == 2) { // -R
+			block_size = 0x200;
+			ReadEnable = true;
+		}
+		else if (mode == 3) { // -w
+			block_size = 0x210;
+			WriteEnable = true;
+		}
+		else if (mode == 4) { // +w
+			PatchECCEnable = true;
+			block_size = 0x210;
+			WriteEnable = true;
+		}
+		else if (mode == 5) { // -e
+			EraseEnable = true;
 		}
 
-		if( Wrong_arg )
-			break;
-	}
+		//////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////
-
-	if( Wrong_arg || ( !EraseEnable && strlen(FileName)==0 ) ) {
-		printf( "Useage:\n" );
-		printf( "Squirter dev: -r# Filename ->Read\n" );
-		printf( "Squirter dev: -w# Filename ->Write\n" );
-		printf( "Squirter dev: -e#          ->Erase\n" );
-		printf( "\n" );
-		printf( "dev: is hardware interface ftdi:\n" );
-		printf( "# is nand size (16, 64, 256, 512) in MegaBytes\n" );
-		printf( "-r# Reads saving file RAW (with ECC)\n" );
-		printf( "-R# Reads saving file without ECC\n" );
-		printf( "-w# Writes RAW (with ECC) file\n" );
-		printf( "+w# Write RAW (with SPARE) file, init SPARE, block numbers, ECC\n" );
-		return -1;		
-	}
-
-	printf( "Init USB->FTDI->SPI\n" );
-	
-	
-	if( !spi_init() ) {
-		printf( "UNABLE TO INIT FTDI\n" );
-		return -2;
-	}
-
-	unsigned int flashConfig = 0;
-	FlashDataInit( (unsigned char*)&flashConfig );
-	 
-	printf( "Flash Config: %#08X \n", flashConfig );
-	
-	if( ((char*)&flashConfig)[0]==((char*)&flashConfig)[1] && 
-		((char*)&flashConfig)[1]==((char*)&flashConfig)[2] && 
-		((char*)&flashConfig)[2]==((char*)&flashConfig)[3] ) {
-			printf( "BAD CONNECTION TO NAND\n" );
-			return -3;
-	}
-
-	if( SFC_init( flashConfig ) != OK ) {
-		printf( "ERROR:UNKNOWN NAND" );
-		return -4;
-	}
-
-	if( EraseEnable ) {
-		for( block_flash=StartBlock; block_flash<block_flash_max; block_flash+=Sfc.PageCountInBlock ) {
-			FlashDataErase( block_flash );
-			PRINT_BLOCKS;
-		}
-		PRINT_BLOCKS;
-	}
-	else if( WriteEnable ) {
-		FILE *fileW = fopen( FileName, "rb" );
-
-		if( fileW == NULL ) {
-			printf( "File NOT FOUND: %s", FileName );
-			return -8;
+		if (!spi_init()) {
+			return -2; // UNABLE TO INIT FTDI
 		}
 
-		flash_xbox = (unsigned char*)malloc( block_flash_max*528 );
+		flashConfig = 0;
+		FlashDataInit((unsigned char*)&flashConfig);
 
-		if( flash_xbox==NULL ) {
-			printf( "ERROR: NO MEMORY\n" );
-			return -9;
+		if (((char*)&flashConfig)[0] == ((char*)&flashConfig)[1] &&
+			((char*)&flashConfig)[1] == ((char*)&flashConfig)[2] &&
+			((char*)&flashConfig)[2] == ((char*)&flashConfig)[3]) {
+			return -3; // BAD CONNECTION TO NAND
 		}
 
-		fseek (fileW , 0L , SEEK_END);
-		int File_Size = ftell (fileW);
-		fseek (fileW , 0L , SEEK_SET);
+		if (SFC_init(flashConfig) != OK) {
+			return -4; // UNKNOWN NAND
+		}
 
-		unsigned int File_Blocks = File_Size/block_size;
-		unsigned int End_Block = MIN(block_flash_max,File_Blocks);
+		//////////////////////////////////////////////////////////////////////////////////////////
 
-		memset( flash_xbox, 0, sizeof(flash_xbox) );
+		if (EraseEnable) {
+			for (block_flash = StartBlock; block_flash < block_flash_max; block_flash += Sfc.PageCountInBlock) {
+				FlashDataErase(block_flash);
+				//PRINT_BLOCKS;
+			}
+			//PRINT_BLOCKS;
+			return 0;
+		}
 
-		fseek (fileW , StartBlock*block_size , SEEK_SET);
-		fread( (char*)flash_xbox, block_size, End_Block-StartBlock, fileW );
+		//////////////////////////////////////////////////////////////////////////////////////////
 
-		printf( "Start Write NAND:\n" );	
-		addr_raw = 0;
-		for( block_flash=StartBlock; block_flash<End_Block; ++block_flash ) {
-			if( (block_flash%64)==0 || block_flash==StartBlock ) {
-				PRINT_BLOCKS;
+		else if (WriteEnable) {
+			FILE* fileW = fopen(FileName.c_str(), "rb");
+
+			if (fileW == NULL) {
+				return -8; // NO FILE
 			}
 
-			if( PatchECCEnable )
-				fixSpare_ECC( &flash_xbox[addr_raw], block_flash );
+			flash_xbox = (unsigned char*)malloc(block_flash_max * 528);
 
-			FlashDataWrite( &flash_xbox[addr_raw], block_flash, block_size );
-
-			addr_raw+=block_size;
-		}
-		PRINT_BLOCKS;
-
-		fclose( fileW );
-		free( flash_xbox );
-		return 0;
-	}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-	else if( ReadEnable ) {	
-		FILE *fileR = fopen( FileName, "wb" );
-		if( fileR == NULL ) {
-			printf( "File NOT FOUND: %s", FileName );
-			return -10;
-		}
-
-		flash_xbox = (unsigned char*)malloc( block_flash_max*block_size );
-
-		if( flash_xbox==NULL ) {
-			printf( "ERROR: NO MEMORY\n" );
-			return -11;
-		}
-
-		int byteWrite = fwrite( (char*)flash_xbox, 1, 1, fileR );
-		if( byteWrite != 1 ){
-			printf( "Can't Write on File: %s", FileName );
-			return -11;
-		}
-		fseek (fileR , 0L , SEEK_SET);
-
-		printf( "Start Read NAND:\n" );
-		addr_raw = 0;\
-		for( block_flash=StartBlock; block_flash<block_flash_max; ++block_flash ) {
-			FlashDataRead( &flash_xbox[addr_raw], block_flash, block_size );
-			
-			if( (block_flash%64)==0 && (block_flash!=0) ) {
-				PRINT_BLOCKS;
+			if (flash_xbox == NULL) {
+				return -9; // NO MEMORY
 			}
 
-			addr_raw+=block_size;
+			fseek(fileW, 0L, SEEK_END);
+			int File_Size = ftell(fileW);
+			fseek(fileW, 0L, SEEK_SET);
 
-		}
-		if( addr_raw ) {
+			unsigned int File_Blocks = File_Size / block_size;
+			unsigned int End_Block = MIN(block_flash_max, File_Blocks);
+
+			memset(flash_xbox, 0, sizeof(flash_xbox));
+
+			fseek(fileW, StartBlock * block_size, SEEK_SET);
+			fread((char*)flash_xbox, block_size, End_Block - StartBlock, fileW);
+
 			addr_raw = 0;
-			int byteWrite;
-			for( block_flash=StartBlock; block_flash<block_flash_max; ++block_flash ) {
-				do {
-					fseek (fileR , addr_raw, SEEK_SET);
-					byteWrite = fwrite( (char*)&flash_xbox[addr_raw], 1, block_size, fileR );
-				} while( byteWrite != block_size );
+			for (block_flash = StartBlock; block_flash < End_Block; ++block_flash) {
+				//if ((block_flash % 64) == 0 || block_flash == StartBlock) {
+				//	PRINT_BLOCKS;
+				//}
 
-				addr_raw+=block_size;
+				if (PatchECCEnable)
+					fixSpare_ECC(&flash_xbox[addr_raw], block_flash);
+
+				FlashDataWrite(&flash_xbox[addr_raw], block_flash, block_size);
+
+				addr_raw += block_size;
 			}
+			//PRINT_BLOCKS;
+
+			fclose(fileW);
+			free(flash_xbox);
+			return 0;
 		}
 
-		PRINT_BLOCKS;
- 		fclose( fileR );
-		free( flash_xbox );
+		//////////////////////////////////////////////////////////////////////////////////////////
 
-		return 0;
+		else if (ReadEnable) {
+			FILE* fileR = fopen(FileName.c_str(), "wb");
+			if (fileR == NULL) {
+				return -8; // NO FILE
+			}
+
+			flash_xbox = (unsigned char*)malloc(block_flash_max * block_size);
+
+			if (flash_xbox == NULL) {
+				return -9; // NO MEMORY
+			}
+
+			int byteWrite = fwrite((char*)flash_xbox, 1, 1, fileR);
+			if (byteWrite != 1) {
+				return -11; // COULDN'T OPEN FILE
+			}
+			fseek(fileR, 0L, SEEK_SET);
+
+			addr_raw = 0; \
+			for (block_flash = StartBlock; block_flash < block_flash_max; ++block_flash) {
+				FlashDataRead(&flash_xbox[addr_raw], block_flash, block_size);
+
+				//if ((block_flash % 64) == 0 && (block_flash != 0)) {
+				//	PRINT_BLOCKS;
+				//}
+
+				addr_raw += block_size;
+			}
+			if (addr_raw) {
+				addr_raw = 0;
+				int byteWrite;
+				for (block_flash = StartBlock; block_flash < block_flash_max; ++block_flash) {
+					do {
+						fseek(fileR, addr_raw, SEEK_SET);
+						byteWrite = fwrite((char*)&flash_xbox[addr_raw], 1, block_size, fileR);
+					} while (byteWrite != block_size);
+
+					addr_raw += block_size;
+				}
+			}
+
+			//PRINT_BLOCKS;
+			fclose(fileR);
+			free(flash_xbox);
+
+			return 0;
+		}
+
+		return -10; // NO MODE
 	}
+	__declspec(dllexport) int spiGetBlocks() {
+		return block_flash / 32; // Decimal not hex(block_flash/32)
+	}
+	__declspec(dllexport) int spiGetConfig() {
+		return flashConfig; // Decimal not hex
+	}
+}
 
-	return -10;
+int main()
+{
+	printf("EXE mode unavailable");
+	return 1;
 }
